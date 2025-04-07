7775a15520c1ed849dc35437a6e2998fda68af2f
Tori-jnr <Juliet.Nyakiamo@strathmore.edu>
2025-04-07 18:44:51 +0300

    Update Medicine table.sql

--- Medicine table.sql
+++ Medicine table.sql
@@ -5,20 +5,23 @@ Use  MedicineStock;
 Create table Medicine(
 Medicine_ID int not null,
 Medicine_Name varchar(20),
-Date_Supplied date null,
+Date_Supplied date ENUM('Pending','complete'),
 Expiry_Date date not null,
 Quantity_Available int null,
-Price dec(10.2),
+Price_for_each dec(10.2),
 primary key (Medicine_ID),
 Supplier_Name varchar(100));
 
 describe Medicine;
 Insert into Medicine(Medicine_ID,Medicine_Name,Date_Supplied,Expiry_Date,Quantity_Available,Price)
   Values
-  ('Asprin','Hawa.A','Pending','','',''),
-  ('Cetrizine','Karen.K.','pending','','',''),
-  ('Dextromethorphan','Cherie.D.','complete','','',''),
- ('Fluconazole','Canesten','Pending','','',''
+  ('1','Asprin','Hawa.A','Pending','2026-6-26','150','150.00'),
+  ('2','Cetrizine','Karen.K.','pending','2025-12-5','370','48.00'),
+  ('3','Dextromethorphan','Cherie.D.','complete','2027-3-6','100','900.00'),
+ ('4','Fluconazole','Canesten','Pending','2027-7-20','42','250.00');
+
+Select *from Medicine;
+
 Create table Supplier(
 Supplier_ID int not null,
 Sup_Name varchar(20)not null,
