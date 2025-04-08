97625415b6b957ceab7ac7fcd6b7ded4062ed086
VickyKanyama <146171698+VickyKanyama@users.noreply.github.com>
2025-04-08 15:54:39 +0300

    Update employee table.sql

--- employee table.sql
+++ employee table.sql
@@ -3,19 +3,20 @@ CREATE TABLE Employee (
     Emp_Name VARCHAR(35) NOT NULL,
     Department VARCHAR(100) NOT NULL,
     Phone VARCHAR(20) NOT NULL,
-    Email VARCHAR(100) NOT NULL,
-    Address VARCHAR(150) NOT NULL
+    Email VARCHAR(100) NOT NULL
 );
+
+INSERT INTO Employee (Emp_Name, Department, Phone, Email)
+VALUES
+('Lucy Nduta', 'Optician', '0712345678', 'lucy.nduta@cityhospital.org'),
+('Brian Otieno', 'Vascular Surgery', '0723456789', 'brian.otieno@cityhospital.org'),
+('Cynthia Mwangi', 'Pediatric', '0734567890', 'cynthia.mwangi@cityhospital.org');
 
 ALTER TABLE Employee ADD CONSTRAINT unique_phone UNIQUE (Phone);
 ALTER TABLE Employee ADD CONSTRAINT unique_email UNIQUE (Email);
 
 CREATE INDEX idx_email ON Employee (Email);
-
-INSERT INTO Employee (Emp_Name, Department, Phone, Email, Address) VALUES
-('Lucy Nduta', 'Optician', '0712345678', 'lucy.nduta@cityhospital.org', 'Nairobi Area A'),
-('Brian Otieno', 'Vascular Surgery', '0723456789', 'brian.otieno@cityhospital.org', 'Mombasa Area B'),
-('Cynthia Mwangi', 'Pediatric', '0734567890', 'cynthia.mwangi@cityhospital.org', 'Nakuru Block A');
+SELECT*FROM Employee WHERE Email='brian.otieno@cityhospital.org';
 
 UPDATE Employee
 SET Department = 'Pharmacy'
