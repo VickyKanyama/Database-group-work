1b98c0de1b4b318f0a6c06e892b84e8a530a30a9
Tori-jnr <Juliet.Nyakiamo@strathmore.edu>
2025-04-08 09:31:57 +0300

    Update Medicine table.sql

--- Medicine table.sql
+++ Medicine table.sql
@@ -3,22 +3,22 @@ Create Database MedicineStock;
 Use  MedicineStock;
 
 Create table Medicine(
-Medicine_ID int not null,
+Medicine_ID int NOT NULL,
 Medicine_Name varchar(20),
 Date_Supplied date ENUM('Pending','complete'),
-Expiry_Date date not null,
-Quantity_Available int null,
+Expiry_Date date NOT NULL,
+Quantity_Available int NULL,
 Price_for_each dec(10.2),
 primary key (Medicine_ID),
-Supplier_Name varchar(100));
+Supplier_Name varchar(100) Null);
 
 describe Medicine;
-Insert into Medicine(Medicine_ID,Medicine_Name,Date_Supplied,Expiry_Date,Quantity_Available,Price)
+Insert into Medicine(Medicine_ID,Medicine_Name,Date_Supplied,Expiry_Date,Quantity_Available,Price,Supplier_Name)
   Values
-  ('1','Asprin','Hawa.A','Pending','2026-6-26','150','150.00'),
-  ('2','Cetrizine','Karen.K.','pending','2025-12-5','370','48.00'),
-  ('3','Dextromethorphan','Cherie.D.','complete','2027-3-6','100','900.00'),
- ('4','Fluconazole','Canesten','Pending','2027-7-20','42','250.00');
+  ('1','Asprin','Pending','2026-6-26',150,150.00,''),
+  ('2','Cetrizine','pending','2025-12-5',370,48.00,''),
+  ('3','Dextromethorphan','complete','2027-3-6',100,900.00,''),
+ ('4','Fluconazole','Pending','2027-7-20',42,250.00,'');
 
 Select *from Medicine;
 
